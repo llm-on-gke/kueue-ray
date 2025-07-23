2112401490d3025ef40a5bdb9bbeadebb2806f12
rick chen <admin@rickruguichen.altostrat.com>
2025-07-23 03:29:26 +0000

    tpu ray test

--- tpu-ray/rayjob-script.sh
+++
@@ -1,3 +0,0 @@
-pip install "jax[tpu]" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-python -c 'import jax; print("Global device count:", jax.device_count())'
-sleep 60
--- tpu-ray/submit-rayjob.sh
+++ tpu-ray/submit-rayjob.sh
@@ -3,5 +3,5 @@ export RAY_ADDRESS="http://localhost:8265"
 ray job submit --address="http://localhost:8265" \
     --working-dir . \
     -- \
-    bash rayjob-script.sh
+    python tpu-test.py
     
